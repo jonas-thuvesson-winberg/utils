b64db7c6adc73ca1863e6b9b103937c2cf4528d9
Jonas Thuvesson Winberg <jonasthuvesson@gmail.com>
2022-11-23 20:25:57 +0100

    Remove elixir test stuff

--- scripts/elixir-scripts/script.exs
+++
@@ -1,8 +0,0 @@
-{valid_args, _, _} = OptionParser.parse(System.argv(), strict: [greet: :string])
-
-if valid_args !== [] do
-  [greet: x] = valid_args
-  IO.puts("Hello #{x}")
-else
-  IO.puts("Hello World!")
-end
--- scripts/elixir_hello_world_advanced/test/elixir_hello_world_advanced_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ElixirHelloWorldAdvancedTest do
-  use ExUnit.Case
-  doctest ElixirHelloWorldAdvanced
-
-  test "greets the world" do
-    assert ElixirHelloWorldAdvanced.hello() == :world
-  end
-end
